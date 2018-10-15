70c8ffaf33c3c99cf87cfa6b8fe6c6d8fe873acd
iob764rv <iic287m@gmail.com>
2018-10-15 13:26:49 -0400

    Update output.py

--- output.py
+++ output.py
@@ -6,3 +6,11 @@ if sys.version_info[0] < 3:
 	import Tkinter as Tk 	# for Python 2
 else:
 	import tkinter as Tk   	# for Python 3
+
+print('Click to close')
+
+root = Tk.Tk()      # or 'top'
+
+L2 = Tk.Label(root, text = 'This is another label')
+L2.pack()
+root.mainloop()
