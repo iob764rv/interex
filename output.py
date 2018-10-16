7acf2fe9bc79c5f788b07f09aab24fc157464aef
iob764rv <iic287m@gmail.com>
2018-10-16 11:53:52 -0400

    Update output.py

--- output.py
+++ output.py
@@ -8,7 +8,8 @@ else:
 	import tkinter as Tk   	# for Python 3
 
 print('Click to close')
-
+string="String" #string displayed after click
 root = Tk.Tk()      # top
+L2 = Tk.Label(root, text = string)
 L2.pack()
 root.mainloop()
